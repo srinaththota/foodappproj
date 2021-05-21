344d85ec2babf2dc0ff2ea0c0e52374e8ff83cf8
srinaththota <srinath.thota@gmail.com>
2021-05-22 05:14:17 +0530

    added Meals

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AvailableMeals from './AvailableMeals'
+import MealsSummary from './MealsSummary'
+
+const Meals=()=>{
+    return<>
+    <MealsSummary/>
+    <AvailableMeals/>
+    </>
+}
+
+export default Meals;
